dcc95849fb36c62ba0ad37f57dbe6f5ca115d7f6
Steven <jgu8@u.rochester.edu>
2020-10-29 14:31:13 -0400

    init

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+set POSTGRES_USERNAME=udagramdbuser
+set POSTGRES_PASSWORD=GJK0119kjg
+set POSTGRES_HOST=udagramdbid.c2h4vtaxn9pw.us-east-2.rds.amazonaws.com
+set POSTGRES_DB=udagramdb
+set AWS_BUCKET=udacity3-gu0119-dev
+set AWS_REGION=us-east-2
+set AWS_PROFILE=default
+set JWT_SECRET=hello
+set URL=http://localhost:8100
